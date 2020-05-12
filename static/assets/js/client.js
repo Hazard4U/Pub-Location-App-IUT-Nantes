93eccb9554cce8db26b5a50d664088fe2e2ce9a3
Théo Martel <martel.theo@outlook.com>
2020-05-12 10:31:54 +0200

    Map

--- static/assets/js/client.js
+++ static/assets/js/client.js
@@ -2,14 +2,9 @@ import {getCookies} from "../utils/tools.js";
 
 let socket = io();
 
-let USER_DATA = undefined;
 
 socket.on("connect", () => {
     socket.emit("getSession", getCookies().id);
-});
-
-socket.on("setSession",(data) => {
-    USER_DATA = data;
 });
 
 export {socket};
